454b3d5cea693b771cb061455956b5428ce8020f
Chelsea David <cryan17@cnm.edu>
2018-07-24 08:37:44 -0600

    reflections

---
+++ snap-reflection.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Data Design Reflection</title>
+	</head>
+	<body>
+		<h1>What is the most significant thing you have learned about object oriented?</h1>
+		<p>The most significant thing I have learned about object oriented is the  model first, action second. We essentially are modeling the <em>actors</em> and giving relations and behaviors second. I like to think of it in terms of a <em>hanger system</em>.
+			you have your main hanger with a sticky of what the object is. and from there you have hangers(attributes) hanging on that main hanger (object) and at any point you can grab a hanger from the hanger construction and all its attributes there in.
+			Disclaimer - if this is wrong, it is MDav's fault!
+		</p>
+		<h1>What is the most significant thing you have learned about the front end on the backend?</h1>
+		<p>The most significant thing I have learned would have to be the importance itself. It was said to me once that 10 minutes of 		planning equal 17 weeks of actual work. The more I am learning the ways front end "talk" to backend and vice versa the more i am 		realizing this could not be more true. The importance of backend is crucial as if it is not executed properly, errors will most 		likely be all you see on the front end, if anything. The same goes for frontend. If the front end is not executed properly in 		relation to back end, you will have nowhere for the backend data to go.
+		Again, Disclaimer, if this is wrong, it is MDav's fault.
+		</p>
+		<h1>Why is Encapsulation important?</h1>
+		<p>
+			Encapsulation is important because in OOP, access to state variables can be a problem because it will subvert the behavior. Therefore encapsulation allows the objects state to be altered by setting access levels to each member and method. General strategy is to make the objects state private and methods public
+		</p>
+	</body>
+</html>
